3821ff90f545a8d18b87a3ace4e3d74a08088bb2
cestswoo <162071020+cestswoo@users.noreply.github.com>
2024-06-04 11:09:10 +0900

    Update diary_page.py

--- pages/diary_page.py
+++ pages/diary_page.py
@@ -328,7 +328,7 @@ def main():
  
 # 앱 실행
 with st.sidebar:
-    menu = option_menu("MomE", ['Home','Dashboard','Diary','육아 SNS','To do list', '하루 자가진단', 'LogOut'],
+    menu = option_menu("MomE", ['Home','Dashboard','Diary','MOMents','To do list', '하루 자가진단', 'LogOut'],
                         icons=['bi bi-house-fill','bi bi-grid-1x2-fill','book-half','Bi bi-star-fill','Bi bi-calendar-check' ,'bi bi-capsule-pill', 'box-arrow-in-right'],
                         menu_icon="baby", default_index=2,
                         styles={
@@ -340,7 +340,7 @@ if menu =='Home':
     st.switch_page("pages/home.py")
 elif menu =='Dashboard':
     st.switch_page("pages/dashboard_page.py")
-elif menu == '육아 SNS':
+elif menu == 'MOMents':
     st.switch_page("pages/SNS2.py")
 elif menu == 'To do list':
     st.switch_page("pages/daily_schedule.py")
