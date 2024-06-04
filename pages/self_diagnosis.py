a7ab71d219929ae549a9928ee9aa791f865123ec
cestswoo <162071020+cestswoo@users.noreply.github.com>
2024-06-04 16:06:08 +0900

    Update self_diagnosis.py

--- pages/self_diagnosis.py
+++ pages/self_diagnosis.py
@@ -234,8 +234,8 @@ def main():
 
 # Sidebar menu
 with st.sidebar:
-    menu = option_menu("MomE", ['Home', 'Dashboard', 'Diary', 'MOMents', 'To do list', '하루 자가진단', 'LogOut'],
-                        icons=['bi bi-house-fill', 'bi bi-grid-1x2-fill', 'book-half', 'Bi bi-star-fill', 'Bi bi-calendar-check', 'bi bi-capsule-pill', 'box-arrow-in-right'],
+    menu = option_menu("MomE", ['Home', 'Dashboard', 'Diary', 'MOMents', '하루 자가진단', 'LogOut'],
+                        icons=['bi bi-house-fill', 'bi bi-grid-1x2-fill', 'book-half', 'Bi bi-star-fill', 'bi bi-capsule-pill', 'box-arrow-in-right'],
                         menu_icon="baby", default_index=5,
                         styles={
                             "icon": {"font-size": "23px"},
@@ -251,8 +251,6 @@ with st.sidebar:
         st.switch_page("pages/SNS2.py")
     elif menu == 'Home':
         st.switch_page("pages/home.py")
-    elif menu == 'To do list':
-        st.switch_page("pages/daily_schedule.py")
     elif menu == 'LogOut':
         st.switch_page("dd1.py")
 
