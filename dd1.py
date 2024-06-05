f1389cf909e8100d96cb1c099d76f65bc978779b
cestswoo <162071020+cestswoo@users.noreply.github.com>
2024-06-05 19:06:06 +0900

    Update dd1.py

--- dd1.py
+++ dd1.py
@@ -2,51 +2,6 @@ import streamlit as st
 import hashlib
 import sqlite3
 
-st.markdown(
-    """
-  <link href="https://hangeul.pstatic.net/hangeul_static/css/nanum-square.css" rel="stylesheet">    
-    <style>
-    * {
-        font-family: 'NanumSquare', sans-serif !important;
-    }
-    .stApp{
-        background: #FFF9EF;
-    }
-    .stButton button {
-            background-color: #FFF9EF;
-            border-radius: 12px;
-            padding: 10px 24px;
-            text-align: center;
-            font-size: 20px;
-            cursor: pointer;
-            width: 325px;
-            border: none;
-       }
-    .stButton button:hover {
-        background-color: #091F5B;
-        color: #ffffff;
-       }
-    .stTabs [role="tabpanel"] {
-            background-color: #ffffff; /* Change this to your desired content background color */
-            border-top: none;
-            padding: 20px;
-            border-radius: 0 0 30px 30px;
-            height: 500px;
-        }
-    .stTabs [data-baseweb="tab-list"] 
-    button [data-testid="stMarkdownContainer"] p {
-    font-size:16px;
-    color: #091F5B;
-    font-family: 'NanumSquareAceb', sans-serif !important;
-    }
-     div[data-testid="stTabs"] div[role="tablist"] {
-        background-color: #ffffff;
-        padding: 10px;
-        border-radius: 30px 30px 0px 0px;
-    }
-            </style>""",
-    unsafe_allow_html=True)
-
 conn = sqlite3.connect('data.db')
 c = conn.cursor()
 
@@ -76,6 +31,9 @@ def check_hashes(password,hashed_text):
         return hashed_text
     return False
 
+st.title("MomE")
+st.markdown("<h6 style='margin-top: -8px'>| MomEase : 엄마의 편안함</h2>", unsafe_allow_html=True)
+
 # 세션 상태에 'logged_in' 상태가 없으면 초기화
 if 'logged_in' not in st.session_state:
     st.session_state['logged_in'] = False
@@ -86,50 +44,33 @@ if 'logged_in_user' not in st.session_state:
 #     st.write(f"환영합니다, {st.session_state['logged_in_user']}님!")
 #     st.switch_page("pages/home.py")
 # else:
-tab1, tab2 = st.tabs(["로그인", "회원가입"])
+tab1, tab2 = st.tabs(["Login", "Sign up"])
 
 with tab1:
-    col1, col2 = st.columns(2)
-    with col1:
-        st.image("media/loginImg.png")
-
-    with col2:
-        st.write("")
-        st.write("")
-        st.write("")
-
-        st.subheader("안녕하세요!")
-        st.subheader("MomE입니다.")
-        username = st.text_input("ID")
-        password = st.text_input("Password", type='password')
-        if st.button("로그인"):
-            create_usertable()
-            hashed_pswd = make_hashes(password)
-            result = login_user(username, check_hashes(password, hashed_pswd))
-            if result:
-                st.session_state['logged_in_user'] = username  # Save logged in username in session state
-                st.session_state['logged_in'] = True
-                st.switch_page("pages/home.py")
-                # st.experimental_rerun()  # 페이지 리로드
-            else:
-                st.warning("아이디 혹은 비밀번호가 틀렸습니다!") #고침
+    st.subheader("로그인")
+    username = st.text_input("ID")
+    password = st.text_input("Password", type='password')
+    if st.button("로그인"):
+        create_usertable()
+        hashed_pswd = make_hashes(password)
+        result = login_user(username, check_hashes(password, hashed_pswd))
+        if result:
+            st.session_state['logged_in_user'] = username  # Save logged in username in session state
+            st.session_state['logged_in'] = True
+            st.switch_page("pages/home.py")
+            # st.experimental_rerun()  # 페이지 리로드
+        else:
+            st.warning("아이디/비밀번호가 틀렸습니다!")
 
 with tab2:
-    col3, col4 = st.columns(2)
-    with col3:
-        st.image("media/loginImg.png")
-
-    with col4:
-        st.write("")
-        st.write("")
-        st.write("")
-
-        st.subheader("환영합니다!")
-        new_user = st.text_input("ID", key='ID')
-        new_password = st.text_input("Password", type='password', key="new_password")
+    st.subheader("회원가입")
+    new_user = st.text_input("ID", key='ID')
+    new_password = st.text_input("Password", type='password', key="new_password")
     
-        if st.button("회원가입"):
-            create_usertable()
-            add_userdata(new_user, make_hashes(new_password))
-            st.success("회원가입이 완료되었습니다. 로그인 탭으로 가서 로그인하세요.")
-            # st.info("Go to Login Menu to login")
+    if st.button("회원가입"):
+        create_usertable()
+        add_userdata(new_user, make_hashes(new_password))
+        st.success("회원가입이 완료되었습니다. 로그인 탭으로 가서 로그인하세요.")
+        # st.info("Go to Login Menu to login")
+
+st.image("media/homeImg 1.png")
