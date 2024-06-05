2af50a2bce2a9c299d89587d2cd5537b4c691004
yunhaecho <91577577+yuunhae@users.noreply.github.com>
2024-06-06 05:59:06 +0900

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -75,7 +75,7 @@ st.markdown(
         position: absolute;
         top: 50%;
         width: 707px;
-        height: 2150px;
+        height: 1800px;
         background-color: white;
         border-radius: 30px 30px 30px 30px;
     }
