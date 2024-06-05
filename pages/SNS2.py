484657233c4f0ca380dc674d9f2fe2ca57aeb74e
yunhaecho <91577577+yuunhae@users.noreply.github.com>
2024-06-05 22:19:59 +0900

    Update SNS2.py

--- pages/SNS2.py
+++ pages/SNS2.py
@@ -225,7 +225,7 @@ with st.sidebar:
                         styles={
                             "icon": {"font-size": "23px"},
                             "title": {"font-weight": "bold"},
-                            "nav-link-selected": {"background-color": "#D0E3FF", "color":"#FFF9EF", "font-family":"'NanumSquareAceb', sans-serif !important"},
+                            "nav-link-selected": {"background-color": "#D0E3FF", "color":"#091F5B", "font-family":"'NanumSquareAceb', sans-serif !important"},
                             "container": {"background-color": "#D0E3FF", "color":"#FFF9EF"}
                         })
 
