6f7ce6ee16a2dfa2a202b6e3e4d03316048bfc07
yunhaecho <91577577+yuunhae@users.noreply.github.com>
2024-06-05 21:52:02 +0900

    Update diary_page.py

--- pages/diary_page.py
+++ pages/diary_page.py
@@ -173,7 +173,7 @@ def main():
             padding: 20px;
             border-radius: 0 0 30px 30px;
             box-shadow: 5px 5px 5px #DFDCD5;
-            height: 1100px;
+            height: 1200px;
         }
         .stTabs [data-baseweb="tab-list"] 
             button [data-testid="stMarkdownContainer"] p {
